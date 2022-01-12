f928801d74bbc50480b781868dc59821b526dfd8
Rich-gif <86768287+Rich-gif@users.noreply.github.com>
2022-01-12 10:01:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ var canvas = new fabric.Canvas('myCanvas');
 
 function new_image()
 {
-	fabric.image.fromURL('BirthdayImage.jpg', function(img){
-        block_image_object=Img;
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+    block_image_object=Img;
         
         block_image_object.scaleToWidth(700);
         block_image_object.scaleToHeight(510);
-        block_image_object.set(
-            {
-                top=0;
-                left=0;
+        block_image_object.set({
+                top=0,
+                left=0
             }
         );
-        canvas.add(block_image_object);
-    }
+        canvas.add(block_image_object); });
+    
 	
 }
 
